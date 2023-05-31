f428aeca4762616940b148d98153a166b035a746
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-05-31 17:29:04 +0600

    User info insert to Database

--- index.js
+++ index.js
@@ -26,9 +26,24 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDB").collection("users");
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
+
+    // insert Users to the Database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "User Already Exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Get All Menu
     app.get("/menu", async (req, res) => {
