1022e512774dc0f6b40bb2868b6b796289734d72
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-02 01:57:41 +0600

    Generate JWT Access Token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT | 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connection Url
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8grpiox.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +33,14 @@ async function run() {
     const reviewsCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+    // Post and generate Access Token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
     // get users from database
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -89,6 +99,7 @@ async function run() {
       if (!email) {
         res.send([]);
       }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
