2461f173cb740bf77848465c4acf17bbe6716f28
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-05 08:41:28 +0600

    get and post menu items

--- index.js
+++ index.js
@@ -153,6 +153,14 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    // add item on Menu or Post an Item
+    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
     // Get All Reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
